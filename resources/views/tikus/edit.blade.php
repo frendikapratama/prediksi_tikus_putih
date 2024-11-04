b16156215690a5c5f19752f919fac5d6d4378a0a
frendikapratama <frendikapratama28@gmail.com>
2024-11-04 22:46:49 +0700

    view edit data tikus

--- resources/views/tikus/edit.blade.php
+++ resources/views/tikus/edit.blade.php
@@ -3,42 +3,53 @@
 @section('title', 'Edit Data Tikus')
 
 @section('content')
-    <h1>Edit Data Tikus</h1>
-    <a href="{{ route('tikus.index') }}" class="btn btn-secondary mb-3">Back to Data Tikus</a>
+    <div class="col-12 grid-margin stretch-card">
+        <div class="card">
+            <div class="card-body">
+                <h4 class="card-title">Edit Data Tikus</h4>
+                <form action="{{ route('tikus.update', $tikus->id) }}" method="POST">
+                    @csrf
+                    @method('PUT')
 
-    <form action="{{ route('tikus.update', $tikus->id) }}" method="POST">
-        @csrf
-        @method('PUT')
+                    <div class="form-group">
+                        <label for="jenis_id" class="form-label">Jenis</label>
+                        <select name="jenis_id" id="jenis_id" class="form-control text-light" required>
+                            <option value="">Select Jenis</option>
+                            @foreach ($jenis as $item)
+                                <option value="{{ $item->id }}" {{ $item->id == $tikus->jenis_id ? 'selected' : '' }}>
+                                    {{ $item->name }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>
 
-        <div class="mb-3">
-            <label for="jenis_id" class="form-label">Jenis</label>
-            <select name="jenis_id" id="jenis_id" class="form-select" required>
-                <option value="">Select Jenis</option>
-                @foreach ($jenis as $item)
-                    <option value="{{ $item->id }}" {{ $item->id == $tikus->jenis_id ? 'selected' : '' }}>
-                        {{ $item->name }}
-                    </option>
-                @endforeach
-            </select>
+                    <div class="form-group">
+                        <label for="kategori_size_id" class="form-label">Kategori Size</label>
+                        <select name="kategori_size_id" id="kategori_size_id" class="form-control text-light" required>
+                            <option value="">Select Kategori Size</option>
+                            @foreach ($kategoriSize as $item)
+                                <option value="{{ $item->id }}"
+                                    {{ $item->id == $tikus->kategori_size_id ? 'selected' : '' }}>
+                                    {{ $item->name }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="banyak" class="form-label">Banyak</label>
+                        <input type="number" name="banyak" id="banyak" class="form-control text-light"
+                            value="{{ $tikus->banyak }}" required>
+                    </div>
+
+                    <button type="submit" class="btn btn-primary mr-2">Update Data</button>
+                    <button class="btn btn-dark">
+                        <a href="{{ route('tikus.index') }}" style="text-decoration: none;">
+                            Cancel
+                        </a>
+                    </button>
+                </form>
+            </div>
         </div>
-
-        <div class="mb-3">
-            <label for="kategori_size_id" class="form-label">Kategori Size</label>
-            <select name="kategori_size_id" id="kategori_size_id" class="form-select" required>
-                <option value="">Select Kategori Size</option>
-                @foreach ($kategoriSize as $item)
-                    <option value="{{ $item->id }}" {{ $item->id == $tikus->kategori_size_id ? 'selected' : '' }}>
-                        {{ $item->name }}
-                    </option>
-                @endforeach
-            </select>
-        </div>
-
-        <div class="mb-3">
-            <label for="banyak" class="form-label">Banyak</label>
-            <input type="number" name="banyak" id="banyak" class="form-control" value="{{ $tikus->banyak }}" required>
-        </div>
-
-        <button type="submit" class="btn btn-primary">Update Data</button>
-    </form>
+    </div>
 @endsection
